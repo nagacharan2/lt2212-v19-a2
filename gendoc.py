bc8b44bef7398bf0ee62000a44ca21f66ea0c25e
Naga Charan Meda <gusmedana@GU.GU.SE@mltgpu.flov.gu.se>
2019-02-24 21:49:47 +0100

    initial commit

--- gendoc.py
+++ gendoc.py
@@ -5,11 +5,33 @@ import numpy as np
 import pandas as pd
 from sklearn.decomposition import TruncatedSVD
 from sklearn.feature_extraction.text import TfidfTransformer
+import gensim
+from nltk import RegexpTokenizer
+from nltk.corpus import stopwords
+from os import listdir
+from os.path import isfile, join
 
-# gendoc.py -- Don't forget to put a reasonable amount code comments
-# in so that we better understand what you're doing when we grade!
 
-# add whatever additional imports you may need here
+def nlp_clean(data):
+   new_data = []
+   for d in data:
+      new_str = d.lower()
+      dlist = tokenizer.tokenize(new_str)
+      dlist = list(set(dlist).difference(stopword_set))
+      new_data.append(dlist)
+   return new_data
+
+
+
+class LabeledLineSentence(object):
+    def __init__(self, doc_list, labels_list):
+        self.labels_list = labels_list
+        self.doc_list = doc_list
+    def __iter__(self):
+        for idx, doc in enumerate(self.doc_list):
+              yield gensim.models.doc2vec.LabeledSentence(doc,[self.labels_list[idx]])
+
+
 
 parser = argparse.ArgumentParser(description="Generate term-document matrix.")
 parser.add_argument("-T", "--tfidf", action="store_true", help="Apply tf-idf to the matrix.")
@@ -27,6 +49,36 @@ parser.add_argument("outputfile", type=str,
 args = parser.parse_args()
 
 print("Loading data from directory {}.".format(args.foldername))
+foldername = args.foldername 
+
+#now create a list that contains the name of all the text file in your data #folder
+docLabels_crude = []
+docLabels_grain = []
+
+docLabels_crude = [f for f in listdir(foldername+"crude/") if f.endswith('.txt')]
+docLabels_grain = [f for f in listdir(foldername+"grain/") if f.endswith('.txt')]
+
+tokenizer = RegexpTokenizer(r'\w+')
+stopword_set = set(stopwords.words('english'))
+
+print(len(docLabels_crude),len(docLabels_grain))
+
+#create a list data that stores the content of all text files in order of their names in docLabels
+data_crude = []
+data_grain = []
+for doc in docLabels_crude:
+  data_crude.append(open(foldername+"crude/" + doc).read())
+
+for doc in docLabels_grain:
+  data_grain.append(open(foldername+"grain/" + doc).read())
+
+
+print(data_crude[0])
+
+data_crude = nlp_clean(data_crude)
+data_grain = nlp_clean(data_grain)
+
+print(data_crude[0])
 
 if not args.basedims:
     print("Using full vocabulary.")
