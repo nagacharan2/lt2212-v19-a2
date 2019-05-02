ba383f347a3da21062fa375e33e476e7631efe39
NagaCharan Meda <nagacharan1990@gmail.com>
2019-05-02 10:25:23 +0200

    final

--- simdoc.py
+++ simdoc.py
@@ -4,15 +4,45 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 
-# simdoc.py -- Don't forget to put a reasonable amount code comments
-# in so that we better understand what you're doing when we grade!
 
-# add whatever additional imports you may need here
+def find_cosine(vectorfile):
+    to_csv = pd.read_csv(vectorfile,index_col=0)
+    dicti = to_csv.to_dict('split')
+    nlist = []
+    columns = dicti['columns']
+    index = [i.split('_')[0] for i in dicti['index']]
+    data =  dicti['data']
+    category={}
+    for count,i in enumerate(data):
+        nlist.append((index[count],i))
+    for vectorfile,vector in nlist:
+        if vectorfile  not in category:
+            category[vectorfile]=[vector]
+        else:
+            category[vectorfile]+=[vector]
+    # print(category.keys())
+    sim = cosine_similarity(list(category.values())[0],list(category.values())[1])
+    avg_sim = np.mean(sim)
+    print('Average similarity  ', list(category.keys())[0],list(category.keys())[1],' : ', avg_sim)
+    sim = cosine_similarity(list(category.values())[0],list(category.values())[0])
+    avg_sim = np.mean(sim)
+    print('Average similarity ', list(category.keys())[0],list(category.keys())[0],' : ',avg_sim)
+    sim = cosine_similarity(list(category.values())[1],list(category.values())[0])
+    avg_sim = np.mean(sim)
+    print('Average similarity  ', list(category.keys())[1],list(category.keys())[0],' : ',avg_sim)
+    sim = cosine_similarity(list(category.values())[1],list(category.values())[1])
+    avg_sim = np.mean(sim)
+    print('Average similarity  ', list(category.keys())[1],list(category.keys())[1],' : ',avg_sim)
+    
 
-parser = argparse.ArgumentParser(description="Compute some similarity statistics.")
-parser.add_argument("vectorfile", type=str,
-                    help="The name of the input  file for the matrix data.")
+if __name__ == "__main__":
 
-args = parser.parse_args()
+    parser = argparse.ArgumentParser(description="Compute some similarity statistics.")
+    parser.add_argument("vectorfile", type=str,
+                    help="The name of the input  vectorfile for the matrix data.")
 
-print("Reading matrix from {}.".format(args.vectorfile))
+    args = parser.parse_args()
+
+    print("Reading matrix from {}.".format(args.vectorfile))
+
+    find_cosine(args.vectorfile)
